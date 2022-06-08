e62dc1075fbefa588897d4e2a18c473f7dd759dd
wisline97 <wisdomline.an@gmail.com>
2022-06-08 23:40:12 +0900

    age calculator 1

--- momentum/app.js
+++ momentum/app.js
@@ -1,32 +1,9 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  multi: function (a, b) {
-    return a * b;
-  },
-  powerof: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const addResult = calculator.add(2, 3);
-console.log(addResult);
+console.log(isNaN(age));
 
-const minusResult = calculator.minus(addResult, 3);
-console.log(minusResult);
-
-const divideResult = calculator.divide(12, minusResult);
-console.log(divideResult);
-
-const multiResult = calculator.multi(4, divideResult);
-console.log(multiResult);
-
-const powerofResult = calculator.powerof(multiResult, divideResult);
-console.log(powerofResult);
+if (isNaN(age)) {
+  parseInt(prompt("Please write a number"));
+} else {
+  alert("Thank you for writing your age.");
+}
