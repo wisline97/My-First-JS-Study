1d0c00c76d396bf867137d599b53db69c5381280
wisline97 <wisdomline.an@gmail.com>
2022-06-09 20:44:27 +0900

    update agecalculator

--- momentum/app.js
+++ momentum/app.js
@@ -2,8 +2,16 @@ const age = parseInt(prompt("How old are you?"));
 
 console.log(isNaN(age));
 
-if (isNaN(age)) {
-  parseInt(prompt("Please write a number"));
+if (isNaN(age) || age < 0) {
+  parseInt(prompt("Please write a real positive number"));
+} else if (age < 18) {
+  alert("You are too young");
+} else if (age >= 18 && age <= 50) {
+  alert("You can drink");
+} else if (age > 50 && age <= 80) {
+  alert("You should stop drink");
+} else if (age > 80) {
+  alert("You can do whatever you want.");
 } else {
-  alert("Thank you for writing your age.");
+  alert("You can't drink");
 }
